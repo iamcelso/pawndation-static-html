7cf2e9ed41b2b6e9ff4f543e653fd87413ab4e91
Andrew Lopez <developer.andrew.lopez@gmail.com>
2019-12-11 22:25:25 +0800

    Pawndation - 002/Homepage - added count animation when its visible

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,8 @@
         $navigation = $('nav');
         $mobileNavigation = $('.mobile-navigation');
         $navigationHeight = $navigation.height();
+
+        let isCountingNumberAlreadyShow = false;
         
         $('.featured-items').owlCarousel({
             items: 3,
@@ -50,6 +52,7 @@
 
         $(window).scroll(function(){
             setElementFixed('nav', 50 ,768 , {position : 'absolute', background: '#2e40a0'});
+            animteCountingNumbers('#countingNumberContainer');
         });
 
 //----------------------------------------------------------
@@ -115,5 +118,44 @@
             }
             
         }
+
+        function animteCountingNumbers(selector){
+            if($(selector).isInViewport() && !isCountingNumberAlreadyShow){
+                isCountingNumberAlreadyShow = true;
+                $(selector).children().each(function(){
+                    const element = $(this).find('[data-count]')
+                    let count  = element.text();                   
+                    count = parseInt(count);
+
+                    animateValue(element,1 , count, 2000);
+                });
+            }
+        }
+
+        $.fn.isInViewport = function() {
+            const elementTop = $(this).offset().top;
+            const elementBottom = elementTop + $(this).outerHeight();
+          
+            const viewportTop = $(window).scrollTop();
+            const viewportBottom = viewportTop + $(window).height();
+            return elementBottom > viewportTop && elementTop < viewportBottom;
+          };
+
+        function animateValue(element, start, end, duration) {
+            const range = end - start;
+            const increment = end > start? 1 : -1;
+            const stepTime = Math.abs(Math.floor(duration / range));
+            let current = start;
+
+            const timer = setInterval(function() {
+                current += increment;
+                element.text(current)
+
+                if (current == end) {
+                    clearInterval(timer);
+                }
+            }, stepTime);
+        }
+        
     });
 })(jQuery);
